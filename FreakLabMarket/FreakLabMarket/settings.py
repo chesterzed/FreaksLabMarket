3a533add2cd34ef4bef92953fb522797fb454617
Ratibor Vanin <chester.1.80@mail.ru>
2025-10-13 16:58:06 +0300

    it works on postgreSQL!

--- FreakLabMarket/FreakLabMarket/settings.py
+++ FreakLabMarket/FreakLabMarket/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'FreakLabMarket.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'FreakLab',
+        'USER': 'Chester',
+        'PASSWORD': 'CegthGfhjkm3',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
