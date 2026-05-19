c825b7bba503ed1823e6e9de18d89bd14ab6e436
chesterzed <chester.1.80@mail.ru>
2026-05-19 12:56:39 +0000

    load static

--- FreakLabMarket/FreakLabMarket/settings.py
+++ FreakLabMarket/FreakLabMarket/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+	'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -137,6 +138,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / "media"
